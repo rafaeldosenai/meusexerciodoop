db4a18f86d63291759f6217e677751b0fdfabe1c
Rafael do Senai <rafael_cavalcante150@estudante.sesisenai.org.br>
2025-07-02 14:33:56 -0300

    Update notasEmLetras.js
    waawwawawawawawawawawawwaawawawawawaw

--- notasEmLetras.js
+++ notasEmLetras.js
@@ -3,9 +3,18 @@ let Nota2 = Number(prompt("digite sua segunda nota"))
 
 let media = (Nota1 + Nota2)/2
 
-if(media >= 9 && media <=10){
-    alert("parabéns! você tem um A")
+if(media >= 9 && media <= 10){
+    alert("sua nota foi:"+media+". você tirou um A")
 }
-if(media >= 7.5 && media <9){
-    alert("parabéns!")
+if(media >= 7.5 && media < 9){
+    alert("sua nota foi:"+media+". você tirou um B")
+}
+if(media >= 6.0 && media < 7.5){
+    alert("sua nota foi:"+media+". você tirou um C")
+}
+if(media >= 4.0 && media < 6.0){
+    alert("sua nota foi:"+media+". você tirou um D")
+}
+if(media >= 0 && media < 4.0){
+    alert("sua nota foi:"+media+". você tirou um E")
 }
